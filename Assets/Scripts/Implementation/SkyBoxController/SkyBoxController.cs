0112d1e1609bf7828af6528c6b4f1c0004eb15ac
Mykhailo Blagun <mihaniania25@gmail.com>
2023-12-18 23:57:28 +0200

    [UPD] visual improvements

---
+++ Assets/Scripts/Implementation/SkyBoxController/SkyBoxController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CapsuleSurvival.Impl
+{
+    public class SkyBoxController : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed = 2f;
+
+        private void Update()
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * _rotationSpeed);
+        }
+    }
+}
